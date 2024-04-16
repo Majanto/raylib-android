354303078e5b6e47983f2d48b865be047e9ef371
Majanto <jouany.antonin@gmail.com>
2024-04-17 00:42:07 +0200

    Android NDK version update + improved build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,15 @@
 @echo off
+
 cd raylib/src/
 mingw32-make PLATFORM=PLATFORM_ANDROID
+
+if %errorlevel% neq 0 goto End
+
 cd ../../raylib-game-template/src/
 mingw32-make PLATFORM=PLATFORM_ANDROID
+
+:End
 cd ../..
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo SUCCESS!!
